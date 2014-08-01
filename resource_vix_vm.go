3cffdbf6cf58c6e23c766db6dea2218322c88c47
Camilo Aguilar <camilo.aguilar@gmail.com>
2014-07-31 20:42:10 -0400

    Work in progress

--- resource_vix_vm.go
+++ resource_vix_vm.go
@@ -2,6 +2,7 @@ package terraform_vix
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/c4milo/govix"
 	"github.com/hashicorp/terraform/flatmap"
@@ -36,18 +37,69 @@ func resource_vix_vm_create(
 	d *terraform.ResourceDiff,
 	meta interface{}) (*terraform.ResourceState, error) {
 	//p := meta.(*ResourceProvider)
-	//	client := p.client
+	//client := p.client
 
 	// Merge the diff into the state so that we have all the attributes
 	// properly.
 	rs := s.MergeDiff(d)
 
-	// TODO(c4milo): Get image if it does not exist in ~/.terraform/vix/boxes
-	image, ok := flatmap.Expand(rs.Attributes, "image").([]interface{})
-	if ok {
-		log.Printf("[DEBUG] Image ==> %v", image)
+	name := rs.Attributes["name"]
+	description := rs.Attributes["name"]
+	image := flatmap.Expand(rs.Attributes, "image").([]interface{})
+	cpus, err := strconv.ParseInt(rs.Attributes["cpus"], 0, 16)
+	memory := rs.Attributes["memory"]
+	hwversion, err := strconv.ParseInt(rs.Attributes["hardware_version"], 0, 8)
+	netdrv := rs.Attributes["network_driver"]
+	var networks []string
+
+	if err != nil {
+		return nil, err
 	}
 
+	if raw := flatmap.Expand(rs.Attributes, "networks"); raw != nil {
+		if nets, ok := raw.([]interface{}); ok {
+			for _, net := range nets {
+				str, ok := net.(string)
+				if !ok {
+					continue
+				}
+
+				networks = append(networks, str)
+			}
+		}
+	}
+
+	log.Printf("[DEBUG] networks => %v", networks)
+
+	if len(networks) == 0 {
+		networks = append(networks, "bridged")
+	}
+
+	log.Printf("[DEBUG] Name => %s", name)
+	log.Printf("[DEBUG] Description => %s", description)
+	log.Printf("[DEBUG] image => %v", image)
+	log.Printf("[DEBUG] CPUs => %s", cpus)
+	log.Printf("[DEBUG] Memory => %s", memory)
+	log.Printf("[DEBUG] hwversion => %s", hwversion)
+	log.Printf("[DEBUG] netdrv => %s", netdrv)
+
+	// TODO: Check if there is an image already in ~/.terraform/vix/images/{.Name}
+	// usr, err := user.Current()
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// fmt.Println(usr.HomeDir)
+
+	// path := ""
+	// _, err := os.Stat(filepath.Join(usr.HomeDir, fmt.Sprintf(".terraform/vix/images/%s", ))
+	// if err == nil {
+	// 	return true, nil
+	// }
+	// if os.IsNotExist(err) {
+	// 	return false, nil
+	// }
+
+	// TODO(c4milo): Get image
 	// TODO(c4milo): Check image integrity
 	// TODO(c4milo): Unpack it in ~/.terraform/vix/images. if it does exist, clone the box into images
 	// TODO(c4milo): OpenVM passing vmx path
