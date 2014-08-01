3aae2fcaf1ae9636c274a82ed10363d78c159a8d
Camilo Aguilar <camilo.aguilar@gmail.com>
2014-08-01 11:01:44 -0400

    Adds usage example for shared folders and vswitch resources

--- example.tf
+++ example.tf
@@ -8,9 +8,9 @@ variable "key_path" {
     default = "/Users/camilo/.ssh/id_rsa.pub"
 }
 
-# variable "password" {
-#     default: ""
-# }
+variable "password" {
+     default: ""
+}
 
 
 provider "vix" {
@@ -18,14 +18,20 @@ provider "vix" {
     verify_ssl = false
 }
 
-/*
 resource "vix_vswitch" "vmnet10" {
+    name = "vmnet10"
     nat = true
     dhcp = true
     range = "192.168.1.0/24"
     host_access = true
 }
-*/
+
+resource "vix_sharedfolder" "dev" {
+    name = "dev"
+    guest_path = "/home/camilo/dev"
+    host_path = "/Users/camilo/Development"
+    readonly = false
+}
 
 resource "vix_vm" "coreos" {
     name = "coreos"
@@ -36,14 +42,14 @@ resource "vix_vm" "coreos" {
         checksum = "c791812465f2cda236da1132b9f651cc58d5a7120636e48d82f4cb1546877bbd"
         checksum_type = "sha256"
 
-        # If image is encrypted we need to provide 
-        # password = "${var.password}"
+        # If image is encrypted we need to provide
+        password = "${var.password}"
     }
 
     cpus = 2
     memory = "1g"
     networks = [
-        # "vmnet10",
+        "vmnet10",
         "bridged", 
         # "nat"
     ]
@@ -51,6 +57,9 @@ resource "vix_vm" "coreos" {
     count = 1
     hardware_version = 10
     network_driver = "vmxnet3"
+
+    # Whether to enable or disable shared folders in this VM
+    sharedfolders = "enable"
 
     connection {
         # The default username for our Box image
